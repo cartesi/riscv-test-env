d3931fa7c5d3fd9725351dc2fe26f578eb782335
Eduardo Barthel <eduardo.barthel@cartesi.io>
2022-08-25 12:41:34 -0300

    Add Cartesi-specific changes.
    Make HTIF tohost symbol refer address 0x40008000 without allocating anything.
    Write to tohost with a single 64-bit store.
    Change tohost and fromhost to be aligned to 8-byte boundary and be of type .quad.
    Add a new megapage in the root page table so HTIF can be written to.

--- v/vm.c
+++ v/vm.c
@@ -77,9 +77,10 @@ void wtf()
 # define user_l3pt pt[4]
 # define user_llpt pt[5]
 #elif SATP_MODE_CHOICE == SATP_MODE_SV39
-# define NPT 4
-# define kernel_l2pt pt[2]
-# define user_llpt pt[3]
+# define NPT 5
+# define ext_io_l2pt pt[2]
+# define kernel_l2pt pt[3]
+# define user_llpt pt[4]
 #elif SATP_MODE_CHOICE == SATP_MODE_SV32
 # define NPT 2
 # define user_llpt user_l2pt
@@ -231,7 +232,7 @@ void vm_boot(uintptr_t test_addr)
 
   _Static_assert(SIZEOF_TRAPFRAME_T == sizeof(trapframe_t), "???");
 
-#if (MAX_TEST_PAGES > PTES_PER_PT) || (DRAM_BASE % MEGAPAGE_SIZE) != 0
+#if (MAX_TEST_PAGES > PTES_PER_PT) || (DRAM_BASE % MEGAPAGE_SIZE) != 0 || (EXT_IO_BASE % MEGAPAGE_SIZE) != 0
 # error
 #endif
   // map user to lowermost megapage
@@ -246,6 +247,15 @@ void vm_boot(uintptr_t test_addr)
 #elif SATP_MODE_CHOICE == SATP_MODE_SV39
   l1pt[PTES_PER_PT-1] = ((pte_t)kernel_l2pt >> PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
   kernel_l2pt[PTES_PER_PT-1] = (DRAM_BASE/RISCV_PGSIZE << PTE_PPN_SHIFT) | PTE_V | PTE_R | PTE_W | PTE_X | PTE_A | PTE_D;
+  // map EXT_IO to megapage at appropriate offset in virtual memory
+  #define VPN2(v) ((v >> 2*RISCV_PGLEVEL_BITS+RISCV_PGSHIFT) & (~0ULL >> 64-RISCV_PGLEVEL_BITS))
+  #define VPN1(v) ((v >> RISCV_PGLEVEL_BITS+RISCV_PGSHIFT) & (~0ULL >> 64-RISCV_PGLEVEL_BITS))
+  assert(DRAM_BASE >= EXT_IO_BASE + MEGAPAGE_SIZE);
+  uint64_t dram_vaddr = (((PTES_PER_PT-1) << RISCV_PGLEVEL_BITS) |
+       (PTES_PER_PT-1)) << (RISCV_PGLEVEL_BITS+RISCV_PGSHIFT);
+  uint64_t ext_io_vaddr = dram_vaddr - (DRAM_BASE-EXT_IO_BASE);
+  l1pt[VPN2(ext_io_vaddr)] = ((pte_t)ext_io_l2pt >> PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
+  ext_io_l2pt[VPN1(ext_io_vaddr)] = (EXT_IO_BASE/RISCV_PGSIZE << PTE_PPN_SHIFT) | PTE_V | PTE_R | PTE_W | PTE_X | PTE_A | PTE_D;
   user_l2pt[0] = ((pte_t)user_llpt >> PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
 #elif SATP_MODE_CHOICE == SATP_MODE_SV32
   l1pt[PTES_PER_PT-1] = (DRAM_BASE/RISCV_PGSIZE << PTE_PPN_SHIFT) | PTE_V | PTE_R | PTE_W | PTE_X | PTE_A | PTE_D;
